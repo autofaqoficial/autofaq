1b73b17538ff3332f2df125b39454f467869bd05
teste <vasquesh018@gmail.com>
2025-06-08 12:43:22 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 function toggleSubmenu(event) {
   event.stopPropagation();
   const menu = document.getElementById('submenu');
@@ -8,22 +7,4 @@ function toggleSubmenu(event) {
 document.addEventListener('click', function () {
   const menu = document.getElementById('submenu');
   if (menu) menu.style.display = 'none';
-});
-
-function mostrarFAQ() {
-  const container = document.getElementById('faq-container');
-  if (container.style.display === 'block') {
-    container.style.display = 'none';
-  } else {
-    container.style.display = 'block';
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-  }
-}
-
-window.addEventListener('message', function(event) {
-  if (event.data === 'fecharFAQ') {
-    const container = document.getElementById('faq-container');
-    if (container) container.style.display = 'none';
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
 });
