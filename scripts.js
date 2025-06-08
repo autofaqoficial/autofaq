62bb785dde2d14536d5fb08d385342dccce72462
teste <vasquesh018@gmail.com>
2025-06-08 12:19:55 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 function toggleSubmenu(event) {
   event.stopPropagation();
   const menu = document.getElementById('submenu');
@@ -7,4 +8,22 @@ function toggleSubmenu(event) {
 document.addEventListener('click', function () {
   const menu = document.getElementById('submenu');
   if (menu) menu.style.display = 'none';
+});
+
+function mostrarFAQ() {
+  const container = document.getElementById('faq-container');
+  if (container.style.display === 'block') {
+    container.style.display = 'none';
+  } else {
+    container.style.display = 'block';
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+  }
+}
+
+window.addEventListener('message', function(event) {
+  if (event.data === 'fecharFAQ') {
+    const container = document.getElementById('faq-container');
+    if (container) container.style.display = 'none';
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 });
